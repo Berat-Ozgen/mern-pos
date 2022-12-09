e901c612f599e71781b67dcdf564a5ff033f91c3
Berat Özgen <102038617+Berat-Ozgen@users.noreply.github.com>
2022-12-09 23:33:06 +0300

    CartTotals and Products create
    CartTotals and Products create

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
+import CartTotals from "./components/cart/Card";
 import Categories from "./components/categories/Categories";
 import Header from "./components/header/Header";
+import Products from "./components/products/Products";
 
 const App = () => {
   return (
@@ -7,14 +9,16 @@ const App = () => {
       <Header />
 
       <div className="home px-6 flex justify-between gap-10">
-        <div className="categories flex-1 overflow-auto max-h-[calc(100vh-_-112px)] pb-64">
+        <div className="categories  overflow-auto max-h-[calc(100vh-_-112px)] pb-64">
           <Categories />
         </div>
         <div className="products flex-[8]">
-          <div className="">products</div>
+          <Products />
         </div>
-        <div className="">
-          <div>cart totals</div>
+        <div className="cart-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border">
+          <div>
+            <CartTotals />
+          </div>
         </div>
       </div>
     </>
