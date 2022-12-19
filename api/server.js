ed5ff6761c5ee9db7963fb0cf1ea8ea422358dba
Berat Özgen <102038617+Berat-Ozgen@users.noreply.github.com>
2022-12-19 22:36:16 +0300

    mongoose setup
    mongoose setup

--- api/server.js
+++ api/server.js
@@ -1,6 +1,21 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("connected to MongoDB");
+  } catch (error) {
+    console.log("Failed to connect");
+  }
+};
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+  connect();
+});
