b3ddf0f3aec946bcda5edc8e9affb1be00c4947b
Berat Özgen <102038617+Berat-Ozgen@users.noreply.github.com>
2022-12-19 20:59:58 +0300

    express setup
    express setup

--- api/server.js
+++ api/server.js
@@ -1,1 +1,6 @@
-console.log("berat");
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
