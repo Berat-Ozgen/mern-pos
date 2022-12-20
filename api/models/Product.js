31d76040047c044c446703720244177a6b623268
Berat Özgen <102038617+Berat-Ozgen@users.noreply.github.com>
2022-12-21 01:35:41 +0300

    product model db timestamps
    product model db timestamps

--- api/models/Product.js
+++ api/models/Product.js
@@ -1,23 +1,26 @@
 const mongoose = require("mongoose");
 
-const ProductSchema = mongoose.Schema({
-  title: {
-    Type: String,
-    require: true,
+const ProductSchema = mongoose.Schema(
+  {
+    title: {
+      Type: String,
+      require: true,
+    },
+    img: {
+      Type: String,
+      require: true,
+    },
+    price: {
+      Type: Number,
+      require: true,
+    },
+    category: {
+      Type: String,
+      require: true,
+    },
   },
-  img: {
-    Type: String,
-    require: true,
-  },
-  price: {
-    Type: Number,
-    require: true,
-  },
-  category: {
-    Type: String,
-    require: true,
-  },
-});
+  { timestamps: true }
+);
 
 const Product = mongoose.model("products", ProductSchema);
 module.exports = Product;
