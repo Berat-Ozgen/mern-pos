9f94edc5016734da75cf92e7b18a3e554606a6fa
Berat Özgen <102038617+Berat-Ozgen@users.noreply.github.com>
2022-12-21 19:29:42 +0300

    category crud db
    category crud db

---
+++ api/routes/categories.js
@@ -0,0 +1,48 @@
+const { response } = require("express");
+const Category = require("../models/Category.js");
+const router = require("express").Router();
+
+// tüm kategorileri getir
+router.get("/get-all", async (req, res) => {
+  try {
+    const categories = await Category.find();
+    res.status(200).json(categories);
+  } catch (error) {
+    res.status(400).json("hata");
+  }
+});
+
+// yeni kategori ekle
+router.post("/add-category", async (req, res) => {
+  try {
+    const newCategory = new Category(req.body);
+    await newCategory.save();
+    res
+      .status(200)
+      .json(`${newCategory.title} kategorilere  başarılı bir şekilde eklendi`);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+// kategoriyi güncelleme
+router.put("/update-category", async (req, res) => {
+  try {
+    await Category.findOneAndUpdate(req.body.categoryId, req.body);
+    res.status(200).json("başarılı bir şekilde güncellendi");
+  } catch (error) {
+    res.status(400).json("hata");
+  }
+});
+
+// kategoriyi silme
+router.delete("/delete-category", async (req, res) => {
+  try {
+    await Category.findOneAndDelete(req.body.categoryId);
+    res.status(200).json("başarılı bir şekilde silindi");
+  } catch (error) {
+    res.status(400).json("hata");
+  }
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+
+const app = express();
+const cors = require("cors");
+
 dotenv.config();
+
+// routes
+const categoryRoute = require("./routes/categories");
+
+const PORT = process.env.PORT || 5000;
 
 const connect = async () => {
   try {
@@ -12,8 +21,11 @@ const connect = async () => {
   }
 };
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/category", categoryRoute);
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
