b5ae4b330b82b16ef08dcd255b6184d5b7d66e96
Berat Ozgen <berat.ozgnn.01@gmail.com>
2023-01-24 23:27:18 +0300

    route user.js
     get all User &&  get a user

--- api/server.js
+++ api/server.js
@@ -12,6 +12,7 @@ const categoryRoute = require("./routes/categories");
 const productRoute = require("./routes/products.js");
 const billRoute = require("./routes/bills.js");
 const authRoute = require("./routes/auth.js");
+const userRoute = require("./routes/users.js");
 
 const PORT = process.env.PORT || 5000;
 
@@ -32,6 +33,7 @@ app.use("/api/category", categoryRoute);
 app.use("/api/products", productRoute);
 app.use("/api/bills", billRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
